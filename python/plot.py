b2752477828a3136f4c833dcf338ab144a91131a
H T M A Riyadh <s8htriya@stud.uni-saarland.de>
2020-06-30 03:16:52 +0200

    Python code for reading serial data and plotting

--- python/plot.py
+++ python/plot.py
@@ -1,2 +1,24 @@
-import mathplot as plt
+import serial as sr
+import matplotlib.pyplot as plt
 import numpy as np
+
+s = sr.Serial("COM6", 9600)
+plt.close("all")
+plt.figure()
+plt.ion()
+plt.show()
+
+data = np.array([])
+
+i = 0
+while True:
+    a = s.readline().decode()
+    b = float(a)
+    data = np.append(data, b)
+    print(b)
+    plt.cla()
+    plt.plot(data)
+    plt.pause(0.01)
+    i = i+1
+
+s.close()
